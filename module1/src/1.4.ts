f5a79a5d6283a6291a1c277e3c91719fbd7dc010
topurayhan007 <topurayhan007@gmail.com>
2023-11-02 14:27:35 +0600

    data types continued

---
+++ module1/src/1.4.ts
@@ -0,0 +1,39 @@
+// Basic Data Type
+
+// string
+
+// implicit data type
+let firstName = "topu";
+
+// explicit data type
+let lastName: string = "mamud";
+
+// number
+let roll: number = 123;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+let d: number;
+
+d = 123;
+// d = "ddd";
+// d = true;
+
+// Array
+let friends: string[] = ["kareeb", "shahriar"];
+
+let eligibleRollList: number[] = [1, 3];
+
+// eligibleRollList.push("dd");
+
+// tuple --> array --> order --> type of values
+let coordinates: [number, number] = [1, 5];
+
+let ageName = [50, "Mr.X"]
